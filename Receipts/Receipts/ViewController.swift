cb3cc40df87a51dc548703abc3be88a8586f421e
Jaiden Chicote <jedezzio@gmail.com>
2020-12-17 12:43:03 +1100

    Project Initialisation

---
+++ Receipts/Receipts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Receipts
+//
+//  Created by Jaiden Chicote on 12/17/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Receipts/ReceiptsTests/ReceiptsTests.swift
@@ -0,0 +1,33 @@
+//
+//  ReceiptsTests.swift
+//  ReceiptsTests
+//
+//  Created by Jaiden Chicote on 12/17/20.
+//
+
+import XCTest
+@testable import Receipts
+
+class ReceiptsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
